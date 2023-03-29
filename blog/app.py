4a59ae6d8a94041a02e471362b6ec3621a612f57
GamBit1986 <mazepa86@gmail.com>
2023-03-29 14:55:49 +0300

    Initial commit

---
+++ blog/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from blog.user.views import user
+from blog.articles.views import articles
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    register_blueprints(app)
+    return app
+
+
+def register_blueprints(app: Flask):
+    app.register_blueprint(user)
+    app.register_blueprint(articles)
