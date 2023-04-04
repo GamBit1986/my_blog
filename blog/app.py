35643d6bd026b90b7c45a59de0cd87057933b5c3
GamBit1986 <mazepa86@gmail.com>
2023-04-04 21:56:30 +0300

    lesson 3 is ready

--- blog/app.py
+++ blog/app.py
@@ -1,14 +1,37 @@
 from flask import Flask
+
 from blog.user.views import user
 from blog.articles.views import articles
+from blog.auth.views import auth
+from .models import User
+from .extension import db, login_manager
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = "4q-z85(2n1r=i+lp7-n%q40abiib(+_5f=3ilwnulq_2^+mqgi"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+    app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+    app.config["DEBUG"] = True
+
+    register_extensions(app)
     register_blueprints(app)
+
     return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
 
 def register_blueprints(app: Flask):
     app.register_blueprint(user)
     app.register_blueprint(articles)
+    app.register_blueprint(auth)
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,25 @@
-from blog.app import create_app
+from blog.app import create_app, db
+from werkzeug.security import generate_password_hash
 
 
 app = create_app()
+
+
+@app.cli.command("init-db")
+def init_db():
+    db.create_all()
+
+
+@app.cli.command("create-users")
+def create_users():
+    from blog.models import User
+
+    db.session.add(User(email="aaaa@test.com", password=generate_password_hash("test")))
+    db.session.add(
+        User(email="test2@test.com", password=generate_password_hash("test2"))
+    )
+    db.session.add(
+        User(email="test3@test.com", password=generate_password_hash("test3"))
+    )
+
+    db.session.commit()
